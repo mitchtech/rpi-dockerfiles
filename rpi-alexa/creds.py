6cf494dbdfbb71e294ef8229b6dbf6263606b3d4
Michael J. Mitchell <michael@mitchtech.net>
2016-03-04 20:38:38 -0800

    example amazon developer credentials file

---
+++ rpi-alexa/creds.py
@@ -0,0 +1,11 @@
+import os
+
+#Alexa Settings
+ProductID = ""
+Security_Profile_Description =""
+Security_Profile_ID  = ""
+Client_ID = ""
+Client_Secret = ""
+
+# Alexa Refresh Token
+refresh_token = ''
