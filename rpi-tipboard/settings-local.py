165f7d375c653c79278bae40ca518173aadb5044
Michael J. Mitchell <michael@mitchtech.net>
2016-03-23 04:01:39 +0000

    import rpi-tipboard image

---
+++ rpi-tipboard/settings-local.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+PROJECT_NAME = '[PROJECT_NAME]'
+API_KEY = '[YOUR_PROTECTED_KEY]'
